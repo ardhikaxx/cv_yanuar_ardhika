20c48b947fe449554b0c422bda0baa18aa910eac
ardhikaxx <ardhikayanuar58@gmail.com>
2024-12-05 21:31:16 +0700

    update- pada tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = process.env.NODE_ENV === 'production' ? {} : require('tailwindcss/defaultTheme')
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/flowbite/**/*.js'],
 	daisyui: {
